147476eb4b67a97a163e1ce207ad195efd9b4914
Will Scott <wscottstudent@gmail.com>
2022-07-08 10:18:37 -0600

    fixed issue where saved images would fail due to file naming problems

--- User Side Code Separate Train and Test Same Epoch Different Will Version LOW PASS FILTER TEST.py
+++ User Side Code Separate Train and Test Same Epoch Different Will Version LOW PASS FILTER TEST.py
@@ -47,145 +47,151 @@ directories_list = ['HVC_ra_x_i_data_2016_2019/50 KhZ Recordings - 06_08_16/50Kh
 # use_neuron_index = 3
 for lmn in range(1, 5):
     for ijk in θ[60:71]:
-        #neuron 52 from 2016 (HVC_RA)
-        use_neuron_index = 0
-        # train 1, 14, test 16 (34 looks same as 1)
-        loaded_V_1 = np.loadtxt(directories_list[use_neuron_index]+'nidaq_Dev1_ai-0001_voltage.txt')
-        loaded_I_1 = np.loadtxt(directories_list[use_neuron_index]+'nidaq_Dev1_ai-0001_current.txt')
+        if (ijk != 0):
+            #neuron 52 from 2016 (HVC_RA)
+            use_neuron_index = 0
+            # train 1, 14, test 16 (34 looks same as 1)
+            loaded_V_1 = np.loadtxt(directories_list[use_neuron_index]+'nidaq_Dev1_ai-0001_voltage.txt')
+            loaded_I_1 = np.loadtxt(directories_list[use_neuron_index]+'nidaq_Dev1_ai-0001_current.txt')
 
-        fc_tau = 1/ (2 * np.pi * ijk)
+            fc_tau = ijk
+            print("lpf tau = "+str(fc_tau)+"\n")
 
-        Current_lps = calc_Fourier_power_spectrum(loaded_I_1, [i * (1/50000) for i in range(len(loaded_I_1))])
+            Current_lpf = calc_Fourier_power_spectrum(np.array([loaded_I_1, loaded_V_1]), np.array([[i * (1/50000)] for i in range(len(loaded_I_1))]))
 
-        for h in range(len(loaded_I_1)):
-            if Current_lps[h][1] >= fc_tau:
-                fc = h
-                break
+            h_final = 0
 
-        b = lmn * 0.001 #transition band
-        N = int(np.ceil((4 / b)))
-        
-        if not N % 2: N += 1
-        n = np.arange(N)
-        
-        sinc_func = np.sinc(2 * fc * (n - (N - 1) / 2.))
-        window = 0.42 - 0.5 * np.cos(2 * np.pi * n / (N - 1)) + 0.08 * np.cos(4 * np.pi * n / (N - 1))
-        sinc_func = sinc_func * window
-        sinc_func = sinc_func / np.sum(sinc_func)
-        
-        Current_train = np.convolve(loaded_I_1, sinc_func, "same")[:60000] #loaded_I_1[:60000] #
-        Current_test = np.convolve(loaded_I_1, sinc_func, "same")[60000:120000] #loaded_I_1[60000:120000] #
-        
-        Voltage_train = loaded_V_1[:60000]
-        Voltage_test = loaded_V_1[60000:120000]
-        length = Voltage_train.shape[0]-1000
-        PreLength = Voltage_test.shape[0]-1000
-        print(Voltage_test.shape)
-        #neuron 57 from 2016 (HVC_x)
-        # use_neuron_index = 1
-        
-        # still neuron 57 from 2016 (HVC-x)
-        
-        
-        #neuron 61 from 2016 (HVC_I)
-        # use_neuron_index = 2
-        
-        print("Here 1")
-        print(np.shape(Voltage_train))
-        print(np.shape(Current_train))
-        print(np.shape(Current_test))
-        # Voltage_train = np.loadtxt(directories_list[use_neuron_index]+'nidaq_Dev1_ai-00014_voltage.txt')
-        # Current_train = np.loadtxt(directories_list[use_neuron_index]+'nidaq_Dev1_ai-00014_current.txt')
-        print("Here 2")
-        print(np.shape(Voltage_train))
-        print(np.shape(Current_train))
-        
-        
-        plt.figure()
-        plt.plot(Voltage_train, color='blue')
-        # plt.xlim((30000,50000))
-        plt.title("Training Voltage")
-        plt.ylabel("Voltage (mV)")
-        plt.xlabel("Timestep in file (each timestep = 0.02ms)")
-        # plt.savefig(str(neuron_name_list[use_neuron_index])+"Train 1 14 Test 16"+" Training Voltage")
-        plt.close()
-        plt.figure()
-        plt.plot(Current_train, color='blue')
-        plt.xlim((30000,50000))
-        plt.title("Training Current")
-        plt.ylabel("Current")
-        plt.xlabel("Timestep in file (each timestep = 0.02ms)")
-        # plt.savefig(str(neuron_name_list[use_neuron_index])+"Train 1 14 Test 16"+" Training Current")
-        plt.close()
-        
-        plt.figure()
-        plt.plot(Voltage_test, color='orange')
-        # plt.xlim((30000,50000))
-        plt.title("Test Voltage")
-        plt.ylabel("Voltage (mV)")
-        plt.xlabel("Timestep in file (each timestep = 0.02ms)")
-        # plt.savefig(str(neuron_name_list[use_neuron_index])+"Train 1 14 Test 16"+" Test Voltage")
-        plt.close()
-        plt.figure()
-        plt.plot(Current_test, color='orange')
-        plt.xlim((10000,60000))
-        plt.title("Test Current")
-        plt.ylabel("Current")
-        plt.xlabel("Timestep in file (each timestep = 0.02ms)")
-        # plt.savefig(str(neuron_name_list[use_neuron_index])+"Train 1 14 Test 16"+" Test Current")
-        plt.close()
-        
-        print("DONE HERE")
-        # In[4]:
-        
-        
-        #tau = 3
-        #D = 3
-        for tau in range(5, 10):
-            for D in range(8,12):
-                Xdata = Voltage_train
-                TT = 0.02
-                NoCenters_no_thresh = 500
-                NoCenters_above_thresh = 50
-                DDF = Gauss()
-                # Combine centers above threshold with centers determined by kmeans
-                Centers_k_means = DDF.KmeanCenter(Xdata,NoCenters_no_thresh,D,length,tau);
-                temp_array = copy.deepcopy(Xdata)
-                temp_array[temp_array<-50]=-100
-                Centers_above_thresh = DDF.KmeanCenter(temp_array,NoCenters_above_thresh,D,length,tau);
-                # Center = np.concatenate((Centers_k_means,Centers_above_thresh),axis=0)
-                Center = Centers_k_means
-        
-                print("C_km:"+str(Centers_k_means.shape))
-                # times_where_V_gt_n75 = train_t_ind_arr[V_Train_delayed[0]>=-40]
-                # V_Train_delayed_add = V_Train_delayed[:,(times_where_V_gt_n75-train_t_ind_arr[0])] # important to subtract train_t_ind_arr[0] (time training index vs total time index of V delayed array)
-                # Centers_above_thresh = DDF.KmeanCenter(Xdata[Xdata>-50],100,D,length,tau);
-        
-        
-                print("temp_arry:"+str(temp_array))
-                print("C_at:"+str(Centers_above_thresh.shape))
-        
-                NoCenters = np.shape(Center)[0]
-                print(NoCenters)
-                print("Centers:"+str(Center.shape))
-                np.savetxt('centers/Center '+neuron_name_list[use_neuron_index]+'(D,NumCenters)='+str((D,NoCenters))+'.txt',Center)
-                Center = np.loadtxt('centers/Center '+neuron_name_list[use_neuron_index]+'(D,NumCenters)='+str((D,NoCenters))+'.txt')
-                # Center = np.load('Example/Centers_train25k_5k_D'+str(D)+'_tau'+str(tau)+'.npy')
-        
-                stim_train = Current_train
-                Pdata = Voltage_test
-                beta_arr,R_arr = [10e-8,10e-4],[10e-5,10e-4,10e-3]#[10e-9,10e-8,10e-7,10e-6,10e-5,10e-4,10e-3,10e-2,10e-1]#[10e-4,10e-3,10e-2,10e-1,10e0,10e1,10e2],[10e-4,10e-3,10e-2,10e-1,10e0,10e1,10e2]#,[10e-3,10e-2,10e-1,10e0,10e1,10e2,10e3]
-                bias = 100 # should be larger than tau*(D-1) or something like that
-                X = np.arange(bias,bias+PreLength*TT,TT)
-                stim_test = Current_test
-        
-        
-                beta = 10e-2
-                R = 10e-8
-                title = neuron_name_list[use_neuron_index]+' with 0.02 time step, D = '+str(D)+', Beta = '+str("{:.1e}".format(beta))+', R = '+str("{:.1e}".format(R))+' Train Time = '+str(length)+', Centers = '+str(NoCenters)+', tau = '+str(tau)+', cutoff point ='+str(fc)+', transition band ='+str(b)
-                print(R)
-                F = DDF.FuncApproxF(Xdata,length,Center,beta,R,D,stim_train,tau)
-                try:
+            for h in range(len(loaded_I_1)):
+                if Current_lpf[1][h] > 0 and fc_tau <= 2 * np.pi / Current_lpf[1][h]:
+                    fc = h * 0.001
+                    h_final = h
+                    print("lpf cutoff frequency = "+str(Current_lpf[1][h])+"Hz, at entry number "+str(h)+" with a tau of "+str(2 * np.pi / Current_lpf[1][h])+"\n")
+                    break
+
+            b = lmn * 0.001 #transition band
+            N = int(np.ceil((4 / b)))
+
+            if not N % 2: N += 1
+            n = np.arange(N)
+
+            sinc_func = np.sinc(2 * fc * (n - (N - 1) / 2.))
+            window = 0.42 - 0.5 * np.cos(2 * np.pi * n / (N - 1)) + 0.08 * np.cos(4 * np.pi * n / (N - 1))
+            sinc_func = sinc_func * window
+            sinc_func = sinc_func / np.sum(sinc_func)
+
+            Current_train = np.convolve(loaded_I_1, sinc_func, "same")[:60000] #loaded_I_1[:60000] #
+            Current_test = np.convolve(loaded_I_1, sinc_func, "same")[60000:120000] #loaded_I_1[60000:120000] #
+
+            Voltage_train = loaded_V_1[:60000]
+            Voltage_test = loaded_V_1[60000:120000]
+            length = Voltage_train.shape[0]-1000
+            PreLength = Voltage_test.shape[0]-1000
+            print(Voltage_test.shape)
+            #neuron 57 from 2016 (HVC_x)
+            # use_neuron_index = 1
+
+            # still neuron 57 from 2016 (HVC-x)
+
+
+            #neuron 61 from 2016 (HVC_I)
+            # use_neuron_index = 2
+
+            print("Here 1")
+            print(np.shape(Voltage_train))
+            print(np.shape(Current_train))
+            print(np.shape(Current_test))
+            # Voltage_train = np.loadtxt(directories_list[use_neuron_index]+'nidaq_Dev1_ai-00014_voltage.txt')
+            # Current_train = np.loadtxt(directories_list[use_neuron_index]+'nidaq_Dev1_ai-00014_current.txt')
+            print("Here 2")
+            print(np.shape(Voltage_train))
+            print(np.shape(Current_train))
+
+
+            plt.figure()
+            plt.plot(Voltage_train, color='blue')
+            # plt.xlim((30000,50000))
+            plt.title("Training Voltage")
+            plt.ylabel("Voltage (mV)")
+            plt.xlabel("Timestep in file (each timestep = 0.02ms)")
+            # plt.savefig(str(neuron_name_list[use_neuron_index])+"Train 1 14 Test 16"+" Training Voltage")
+            plt.close()
+            plt.figure()
+            plt.plot(Current_train, color='blue')
+            plt.xlim((30000,50000))
+            plt.title("Training Current")
+            plt.ylabel("Current")
+            plt.xlabel("Timestep in file (each timestep = 0.02ms)")
+            # plt.savefig(str(neuron_name_list[use_neuron_index])+"Train 1 14 Test 16"+" Training Current")
+            plt.close()
+
+            plt.figure()
+            plt.plot(Voltage_test, color='orange')
+            # plt.xlim((30000,50000))
+            plt.title("Test Voltage")
+            plt.ylabel("Voltage (mV)")
+            plt.xlabel("Timestep in file (each timestep = 0.02ms)")
+            # plt.savefig(str(neuron_name_list[use_neuron_index])+"Train 1 14 Test 16"+" Test Voltage")
+            plt.close()
+            plt.figure()
+            plt.plot(Current_test, color='orange')
+            plt.xlim((10000,60000))
+            plt.title("Test Current")
+            plt.ylabel("Current")
+            plt.xlabel("Timestep in file (each timestep = 0.02ms)")
+            # plt.savefig(str(neuron_name_list[use_neuron_index])+"Train 1 14 Test 16"+" Test Current")
+            plt.close()
+
+            print("DONE HERE")
+            # In[4]:
+
+
+            #tau = 3
+            #D = 3
+            for tau in range(5, 10):
+                for D in range(8,12):
+                    Xdata = Voltage_train
+                    TT = 0.02
+                    NoCenters_no_thresh = 500
+                    NoCenters_above_thresh = 50
+                    DDF = Gauss()
+                    # Combine centers above threshold with centers determined by kmeans
+                    Centers_k_means = DDF.KmeanCenter(Xdata,NoCenters_no_thresh,D,length,tau);
+                    temp_array = copy.deepcopy(Xdata)
+                    temp_array[temp_array<-50]=-100
+                    Centers_above_thresh = DDF.KmeanCenter(temp_array,NoCenters_above_thresh,D,length,tau);
+                    # Center = np.concatenate((Centers_k_means,Centers_above_thresh),axis=0)
+                    Center = Centers_k_means
+
+                    print("C_km:"+str(Centers_k_means.shape))
+                    # times_where_V_gt_n75 = train_t_ind_arr[V_Train_delayed[0]>=-40]
+                    # V_Train_delayed_add = V_Train_delayed[:,(times_where_V_gt_n75-train_t_ind_arr[0])] # important to subtract train_t_ind_arr[0] (time training index vs total time index of V delayed array)
+                    # Centers_above_thresh = DDF.KmeanCenter(Xdata[Xdata>-50],100,D,length,tau);
+
+
+                    print("temp_arry:"+str(temp_array))
+                    print("C_at:"+str(Centers_above_thresh.shape))
+
+                    NoCenters = np.shape(Center)[0]
+                    print(NoCenters)
+                    print("Centers:"+str(Center.shape))
+                    np.savetxt('centers/Center '+neuron_name_list[use_neuron_index]+'(D,NumCenters)='+str((D,NoCenters))+'.txt',Center)
+                    Center = np.loadtxt('centers/Center '+neuron_name_list[use_neuron_index]+'(D,NumCenters)='+str((D,NoCenters))+'.txt')
+                    # Center = np.load('Example/Centers_train25k_5k_D'+str(D)+'_tau'+str(tau)+'.npy')
+
+                    stim_train = Current_train
+                    Pdata = Voltage_test
+                    beta_arr,R_arr = [10e-8,10e-4],[10e-5,10e-4,10e-3]#[10e-9,10e-8,10e-7,10e-6,10e-5,10e-4,10e-3,10e-2,10e-1]#[10e-4,10e-3,10e-2,10e-1,10e0,10e1,10e2],[10e-4,10e-3,10e-2,10e-1,10e0,10e1,10e2]#,[10e-3,10e-2,10e-1,10e0,10e1,10e2,10e3]
+                    bias = 150 # should be larger than tau*(D-1) or something like that
+                    X = np.arange(bias,bias+PreLength*TT,TT)
+                    stim_test = Current_test
+
+
+                    beta = 10e-2
+                    R = 10e-8
+                    title = neuron_name_list[use_neuron_index]+' with 0.02 time step, D = '+str(D)+', Beta = '+str("{:.1e}".format(beta))+', R = '+str("{:.1e}".format(R))+' Train Time = '+str(length)+', Centers = '+str(NoCenters)+', tau = '+str(tau)+', lpf tau ='+str(fc_tau)+'cutoff frequency ='+str(1 / (2 * np.pi * fc_tau))+', transition band ='+str(b)
+                    print(R)
+                    F = DDF.FuncApproxF(Xdata,length,Center,beta,R,D,stim_train,tau)
+                    #try:
                     PredValidation = DDF.PredictIntoTheFuture(F,PreLength,stim_test[bias-1:],Pdata[bias-1-(D-1)*tau:])
                     # Tau8
                     plt.figure(figsize=(20,10))
@@ -199,7 +205,7 @@ for lmn in range(1, 5):
                     plt.savefig("Output/"+title+'.png')
                     # plt.show
                     plt.close()
-                    
+
                     '''
                     plt.plot(Voltage_train[:100000])
                     plt.plot(Pdata[0:180000])
@@ -229,8 +235,9 @@ for lmn in range(1, 5):
                     plt.savefig("Output/"+title+'WeightsVSCenterVoltage.png')
                     plt.show()
                     '''
-                except:
-                    print("PredValidation Error\n")
-                
-        
-        
+                    '''
+                    except:
+                        print("PredValidation Error\n")
+                    '''
+
+
