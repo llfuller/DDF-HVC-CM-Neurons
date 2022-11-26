beceabe678002d3878480e1ae5aab95d4facb140
Lawson Fuller <llfuller@users.noreply.github.com>
2022-11-26 13:28:22 -0800

    Allow looping, preparing for import into multifile.py

--- FNN/fnn.py
+++ FNN/fnn.py
@@ -10,6 +10,20 @@ import torch
 import os
 import re
 
+print(torch.cuda.is_available())
+print("")
+print("torch.cuda.device_count()")
+print(torch.cuda.device_count())
+print("")
+print("torch.cuda.current_device()")
+print(torch.cuda.current_device())
+print("")
+print("torch.cuda.device(0)")
+print(torch.cuda.device(0))
+print("")
+print("torch.cuda.get_device_name(0)")
+print(torch.cuda.get_device_name(0))
+
 
 """Define all the functions"""
 def count_fnn(dataset, threshold_R=1e-1):
@@ -37,53 +51,81 @@ def count_fnn(dataset, threshold_R=1e-1):
 
 def generate_min_dist_datapoints(data, window=1000, save_data=True):
     result_data, result_index = [], []
-    for i in tqdm(range(len(data))):
+    print(f"Received data with shape{data.shape}")
+
+    for i in tqdm.tqdm(range(len(data))):
 
         # create a window of data point to search over
         # save the starting index and the ending index of our window as reference
-        start_index, end_index = max(i-window, 0), min(len(data), i+window)
+        start_index, end_index = i-window, i+window#max(i-window, 0), min(len(data), i+window)
 
         # exclude the target point from the window
         search_window = torch.cat([
-            data[start_index:i],
-            data[i+1:end_index]
+            data[(start_index<0)*start_index:0],
+            data[(start_index<0)*0+(start_index>=0)*start_index:i],
+            data[i+1:end_index*(end_index<data.shape[0]) + data.shape[0]*(end_index>=data.shape[0])],
+            data[0:(end_index-data.shape[0])*(end_index>=data.shape[0])]
         ])
         # print(search_window)
 
         # run the distance calculation and find the closest point and their indices
+        # print(f"data[i] is {data[i].shape}")
         distance = torch.norm(search_window - data[i], dim=1)
         # print(search_window - data[i])
         # print(torch.norm(search_window - data[i], dim=1))
         min_distance_index = torch.argmin(distance) # index of minimum distance point inside the window of datapoints
-        print(min_distance_index)
-        min_distance_pair_data = [data[i].data, search_window[min_distance_index].data]
+        # print(min_distance_index)
+        # print(min_distance_index)
+        min_distance_pair_data = [(data[i].data).cpu(), (search_window[min_distance_index].data).cpu()]
 
         # find the real index in respect to the entire dataset
-        real_min_distance_index = start_index + min_distance_index + 1 if start_index + min_distance_index >= i else start_index + min_distance_index
-        min_distance_pair_index = [i, real_min_distance_index.data]
+        real_min_distance_index = None#start_index + min_distance_index + 1 if start_index + min_distance_index >= i else start_index + min_distance_index
+        if start_index + min_distance_index < 0:
+            real_min_distance_index = data.shape[0] + (start_index + min_distance_index)
+        elif (start_index + min_distance_index) >= 0:
+            if (start_index + min_distance_index) < i:
+                real_min_distance_index = start_index + min_distance_index
+            if (start_index + min_distance_index) >= i:
+                if (start_index + min_distance_index) + 1 < data.shape[0]:
+                    real_min_distance_index = start_index + min_distance_index + 1
+                if (start_index + min_distance_index) + 1 >= data.shape[0]:
+                    real_min_distance_index = start_index + min_distance_index + 1 - data.shape[0]
+        else:
+            print(f"Failed to match any case: (start_index + min_distance_index)={(start_index + min_distance_index)} and i={i}")
+        min_distance_pair_index = [i, (real_min_distance_index.data).cpu()]
 
         # save the closest point's index
         # this is the k and j: index of the first and second data points
         result_data.append(min_distance_pair_data)
         result_index.append(min_distance_pair_index)
 
-        # early stopping for testing purposes
-        if i == 0:
+        # early stopping for time-saving purposes
+        if i == int(round(len(data)/10)):
             break
+
+    # for i in tqdm.tqdm(range(len(data))):
+    #     result_data.append(min_distance_pair_data)
+    #     result_index.append(min_distance_pair_index)
+
 
     # convert the list to numpy array
     result_data = np.array(result_data)
     result_index = np.array(result_index)
 
     if save_data:
-        print("Saving results...")
-        np.save(f'min_datapairs_D={result_data.shape[1]}_window={window}_datapoints', result_data)
-        np.save(f'min_datapairs_D={result_index.shape[1]}_window={window}_location', result_index)
+        print(f"Result data has total shape {result_data.shape}")
+        print(f"Saving results for D={data.shape[1]}, window={window}.")
+        np.save(f'data_derived/npy/min_datapairs_D={data.shape[1]}_window={window}_datapoints', result_data)
+        # np.savetxt(f'txt/min_datapairs_D={data.shape[1]}_window={window}_datapoints.txt', result_data)
+        np.save(f'data_derived/npy/min_datapairs_D={data.shape[1]}_window={window}_location', result_index)
+        # np.savetxt(f'txt/min_datapairs_D={data.shape[1]}_window={window}_location', result_index)
         print("Results saved.")
     return result_data, result_index
 
 
 def generate_min_dist_datasets(filepath, tau=5, R_ratio=1e-2, D_arr=np.array([1, 2, 4, 6, 8, 10, 12, 15, 18, 20]), search_window_size=1000, save_data=True):
+
+    # Import data using filepath
     # Column1: Current, Column2: Voltage
     try:
         lilac_114_1_1 = pd.read_csv(filepath, delimiter='\t', header=None)
@@ -93,75 +135,80 @@ def generate_min_dist_datasets(filepath, tau=5, R_ratio=1e-2, D_arr=np.array([1,
     lilac_114_1_1.columns = ['Time', 'Current', 'Voltage']
     lilac_114_1_1.head()
 
-    # create the time delay vectors for each data points with each of the D values
+    # Change stored types of data
     original_data = lilac_114_1_1.to_numpy()
     T = original_data[:, 0].astype(np.int64)
     I = original_data[:, 1].astype(np.float64)
     V_0 = original_data[:, 2].astype(np.float64).reshape((len(T), 1)) # voltage at 0*tau
 
-
+    # create the time delay vectors for each data points with each of the D values
     start = time.time()
     time_delay_datasets = []
     for d in D_arr:
         V_s = np.dstack([np.concatenate([V_0[-i*tau:, :], V_0[:-i*tau, :]], axis=0) for i in range(d+1)])[:, 0, :]
         time_delay_datasets.append(V_s)
+        # print(f"Length of time delay data appended: {V_s.shape}")
     end = time.time()
     print(f"This took {end-start}.")
 
 
     """Search for minimum distance pairs"""
     # change the data from array to tensor for faster calculation
-    torch.device('cuda') # change device index
-    time_delay_datasets = [torch.tensor(arr) for arr in time_delay_datasets]
+    device = torch.device('cuda:0') # change device index
+    time_delay_datasets = [(torch.tensor(arr)).to(device) for arr in time_delay_datasets]
 
     # calculate the min distance result (test) - window=200 will run for 20 mins
     time_delay_datapairs, time_delay_indices = [], []
     for time_delay_data in time_delay_datasets:
+        print(f"Iterating for time delay data with shape {time_delay_data.shape}")
         data, index = generate_min_dist_datapoints(time_delay_data, search_window_size, save_data)
         time_delay_datapairs.append(data)
         time_delay_indices.append(index)
 
-    return time_delay_datasets, time_delay_indices
+    return time_delay_datasets, time_delay_indices, time_delay_datapairs
 
-"""User Defined Parameters: specify filepath, hyperparameters"""
 
-# get target data (Lilac 114, Neuron 1, epoch_1.txt)
-filepath = "./Data2022-50KhZ/7-7-2022/Lilac 114/Neuron 1/epoch_1.txt"
-search_window_size = 100000
-save_data = True
-# define tau (user provided), distance ratio threshold R (user defined hyperparameter), and range of D to search over (trainable parameter)
-tau = 5
-R_ratio = 1e-2
-D_arr = np.array([1, 2, 4, 6, 8, 10, 12, 15, 18, 20])
+def run_this(filepath = "../Data2022-50KhZ/7-7-2022/Lilac 114/Neuron 1/epoch_1.txt"):
+    """User Defined Parameters: specify filepath, hyperparameters"""
 
-"""Run Min-distance pair search"""
-datasets, time_delay_indices = generate_min_dist_datasets(filepath, tau, R_ratio, D_arr, search_window_size, save_data)
+    # get target data (Lilac 114, Neuron 1, epoch_1.txt)
+    search_window_size = 100000
+    save_data = True
+    # define tau (user provided), distance ratio threshold R (user defined hyperparameter), and range of D to search over (trainable parameter)
+    tau = 5
+    R_ratio = 1e-2
+    D_arr = np.array([1, 2, 4, 6, 8, 10, 12, 15, 18, 20])
 
-"""User Defined Parameters: specify root path for loaded files"""
-root = "./FNN/FNN_min-dist_results/"
+    """Run Min-distance pair search"""
+    datasets, time_delay_indices, time_delay_datapairs = generate_min_dist_datasets(filepath, tau, R_ratio, D_arr, search_window_size, save_data)
 
-"""Load Saved min distance datasets"""
-# load from all the npy file
-D_data_dict, D_index_dict = {}, {}
+    """User Defined Parameters: specify root path for loaded files"""
+    root = "../FNN/FNN_min-dist_results/"
 
-try:
-    os.listdir(root)
-except:
-    root = "./FNN/"
+    """Load Saved min distance datasets"""
+    # load from all the npy file
+    D_data_dict, D_index_dict = {}, {}
 
-for filepath in os.listdir(root):
-    if 'min_datapairs' not in filepath:
-        continue
-    # print(filepath)
-    D = int(re.search("D=([0-9]*)", filepath).group(1))
-    window_size = int(re.search("window=([0-9]*)", filepath).group(1))
-    data = True if re.search("datapoints", filepath) else False
-    if data:
-        D_data_dict[(D, window_size)] = np.load(root+filepath)
-    else:
-        D_index_dict[(D, window_size)] = np.load(root+filepath)
+    try:
+        os.listdir(root)
+    except:
+        root = "../FNN/"
 
-"""
-TODO: Run the FNN algorithm here
-- multifile_fps doesn't generate vector for different D_E values, gotta do it for fnn again
-"""
+    for filepath in os.listdir(root):
+        if 'min_datapairs' not in filepath:
+            continue
+        # print(filepath)
+        D = int(re.search("D=([0-9]*)", filepath).group(1))
+        window_size = int(re.search("window=([0-9]*)", filepath).group(1))
+        data = True if re.search("datapoints", filepath) else False
+        if data:
+            D_data_dict[(D, window_size)] = np.load(root+filepath)
+        else:
+            D_index_dict[(D, window_size)] = np.load(root+filepath)
+
+    """
+    TODO: Run the FNN algorithm here
+    - multifile_fps doesn't generate vector for different D_E values, gotta do it for fnn again
+    """
+
+run_this()
